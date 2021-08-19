278ddcf1d8f78c85331a8ac10cf875301c8bd17d
Ligia <mktligia@gmail.com>
2021-08-19 12:49:58 -0400

    create Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_163609) do
+ActiveRecord::Schema.define(version: 2021_08_19_164901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.float "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "semillas", force: :cascade do |t|
     t.string "name"
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_19_163609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
