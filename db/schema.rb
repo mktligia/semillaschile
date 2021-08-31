d0e30f90baa941e1ca57007dab882e5e48c1fe3c
diego obando <daobandop@gmail.com>
2021-08-31 09:52:00 -0400

    avance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_144319) do
+ActiveRecord::Schema.define(version: 2021_08_30_161454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_144319) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
