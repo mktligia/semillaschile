29ecba6420a8ea8db08ae867baf6bd6d13860a53
V. Bruk <vbruk@hey.com>
2021-09-02 11:41:50 -0300

    Actualizar cantidades de productos en la orden

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,12 @@ class OrdersController < ApplicationController
 
   def new
     @product = Product.find(params[:product_id])
+    @cantidad = params[:cantidad]
     @order = current_user.orders.new
   end
 
   def create
     @order = current_user.orders.new(order_params)
-    @product = Product.find(params[:product_id])
     @order.status = "En Proceso"
     @order.line_items.build(order_params[:line_items])
     if @order.save
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:status, :user_id, :user, :total_price_cents, 
-                                    line_items: [:order_id, :amount, :product_id, :total_price_cents])
+                                    line_items: [:order_id, :cantidad, :product_id, :unit_price, :total_price_cents])
   end
 
   def set_order
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,20 @@
 class Order < ApplicationRecord
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   belongs_to :user
+
   monetize :total_price_cents
+
+  after_create_commit :update_total_price
+  after_update_commit :update_total_price
+
+  private
+
+  def update_total_price
+    line_items = self.line_items
+
+    amount = 0
+    line_items.sum(:total_price_cents) if line_items.size > 0
+
+    update(total_price_cents: amount)
+  end
 end
